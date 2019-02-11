4d68ae6250b01ed7240e199f4a2d1d52535e45d2
joepound <14357797+joepound@users.noreply.github.com>
2019-02-11 13:07:09 -0700

    Added server endpoint boilerplates and prepped CRA

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// implement your API here
+const express = require("express");
+const db = require("./data/db");
+const cors = require("cors");
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.post("/api/users", (req, res) => {
+
+});
+
+server.get("/api/users", (req, res) => {
+
+});
+
+server.get("/api/users/:id", (req, res) => {
+
+});
+
+server.delete("/api/users/:id", (req, res) => {
+
+});
+
+server.put("/api/users/:id", (req, res) => {
+
+});
+
+server.listen(5000, () => console.log("Running user server on 5000..."));
