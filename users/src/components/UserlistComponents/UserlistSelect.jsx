cca102f073bad5229ba700e5b718f562cf207c5d
joepound <14357797+joepound@users.noreply.github.com>
2019-02-12 08:36:39 -0700

    Set up UserSelect component to be displayed

--- users/src/components/UserlistComponents/UserlistSelect.jsx
+++ users/src/components/UserlistComponents/UserlistSelect.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function UserlistSelect(props) {
-  return null;
+  return (
+    <select className="userlist__select">
+      <option disabled selected hidden value="">Select a user...</option>
+      <option className="userlist__select__option">User</option>
+    </select>
+  )
 }
 
 export default UserlistSelect;
